e90b727176a3944f53a00e14e722944167e217d2
qianqian001 <qianqian001>
2016-08-14 22:04:49 +0800

    首次提交到本地仓库

---
+++ src/com/test/HelloWorld.java
@@ -0,0 +1,25 @@
+/**
+ * com.test.HelloWorld.java
+ * @author qianlei 2016年8月14日 下午9:56:15
+ */
+package com.test;
+
+
+
+/**
+ * 
+ * @author qianlei 2016年8月14日 下午9:56:15
+ */
+public class HelloWorld {
+
+
+  /**
+   * @param args
+   * @author qianlei 2016年8月14日 下午9:56:16
+   */
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+
+  }
+
+}
